6a19a26d5459438c039545223f52890591f16ff2
JPgitidaho <juanichile2015@gmail.com>
2025-09-12 15:42:50 -0300

    actualization

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,12 +1,7 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart") || [];
-  if (cartItems.length === 0) {
-    document.querySelector(".product-list").innerHTML =
-      "<li>Your cart is empty</li>";
-    return;
-  }
+  const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
@@ -26,6 +21,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
+
   return newItem;
 }
 
--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,23 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
+const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+//function addProductToCart(product) {
+//  setLocalStorage("so-cart", product);
+//}
 // add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+//async function addToCartHandler(e) {
+//const product = await dataSource.findProductById(e.target.dataset.id);
+//addProductToCart(product);
+//}
 
 // add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+//document
+// .getElementById("addToCart")
+//  .addEventListener("click", addToCartHandler);
