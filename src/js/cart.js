2a8360bf9a4674d8342173bdb76ba80d9a3c5d44
JPgitidaho <juanichile2015@gmail.com>
2025-09-13 08:32:59 -0300

    actualization

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,31 +1,28 @@
 import { getLocalStorage } from "./utils.mjs";
 
-function cartItemTemplate(item) {
-  return `
-    <li class="cart-card divider">
-      <a href="#" class="cart-card__image">
-        <img src="${item.Images?.PrimarySmall || item.Images?.PrimaryLarge || "/images/placeholder.png"}" alt="${item.Name}">
-      </a>
-      <a href="#">
-        <h2 class="card__name">${item.Name}</h2>
-      </a>
-      <p class="cart-card__quantity">qty: ${item.quantity || 1}</p>
-      <p class="cart-card__price">$${item.ListPrice}</p>
-    </li>
-  `;
+function renderCartContents() {
+  const cartItems = getLocalStorage("so-cart");
+  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
-function renderCartContents() {
-  const cartElement = document.querySelector("#cart-list");
-  const cart = getLocalStorage("so-cart") || [];
+function cartItemTemplate(item) {
+  const newItem = `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${item.Image}"
+      alt="${item.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${item.FinalPrice}</p>
+</li>`;
 
-  if (cart.length === 0) {
-    cartElement.innerHTML = "<p>Your cart is empty.</p>";
-    return;
-  }
-
-  const htmlItems = cart.map(cartItemTemplate);
-  cartElement.innerHTML = htmlItems.join("");
+  return newItem;
 }
 
 renderCartContents();
--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { getParam } from "./utils.mjs";
+import { getParam, setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
@@ -7,17 +7,19 @@ const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
+//
+function addProductToCart(product) {
+  let cartItems = getLocalStorage("so-cart") || [];
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
+}
 
-//function addProductToCart(product) {
-//  setLocalStorage("so-cart", product);
-//}
-// add to cart button event handler
-//async function addToCartHandler(e) {
-//const product = await dataSource.findProductById(e.target.dataset.id);
-//addProductToCart(product);
-//}
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
 
-// add listener to Add to Cart button
-//document
-// .getElementById("addToCart")
-//  .addEventListener("click", addToCartHandler);
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
+//
