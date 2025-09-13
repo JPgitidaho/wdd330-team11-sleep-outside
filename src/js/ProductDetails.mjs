250b26620e00779ddbd94d4f18edde010d813cfd
JPgitidaho <juanichile2015@gmail.com>
2025-09-13 08:20:18 -0300

    actualization

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,47 +1,38 @@
-import ProductData from "./ProductData.mjs";
-import { getParam } from "./utils.mjs";
-
-function productDetailsTemplate(product) {
-  return `
-    <section class="product-detail">
-      <img src="${product.Images.PrimaryLarge}" alt="${product.Name}">
-      <h2>${product.Name}</h2>
-      <p class="price">$${product.ListPrice}</p>
-      <p>${product.DescriptionHtmlSimple}</p>
-      <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-    </section>
-  `;
-}
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 export default class ProductDetails {
-  constructor(category, dataSource, element) {
-    this.category = category;
+  constructor(productId, dataSource) {
+    this.productId = productId;
     this.dataSource = dataSource;
-    this.element = element;
+    this.product = {};
   }
 
   async init() {
-    const productId = getParam("product");
-    const product = await this.dataSource.findProductById(productId);
-    this.renderProductDetails(product);
-    this.addToCartHandler(product);
+    this.product = await this.dataSource.findProductById(this.productId);
+    this.renderProductDetails();
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", this.addProductToCart.bind(this));
   }
 
-  renderProductDetails(product) {
-    this.element.innerHTML = productDetailsTemplate(product);
+  addProductToCart() {
+    const cartItems = getLocalStorage("so-cart") || [];
+    cartItems.push(this.product);
+    setLocalStorage("so-cart", cartItems);
   }
 
-  addToCartHandler(product) {
-    const button = document.querySelector("#addToCart");
-    button.addEventListener("click", () => {
-      const cart = JSON.parse(localStorage.getItem("so-cart")) || [];
-      const idx = cart.findIndex((i) => i.Id === product.Id);
-      if (idx >= 0) {
-        cart[idx].quantity = (cart[idx].quantity || 1) + 1;
-      } else {
-        cart.push({ ...product, quantity: 1 });
-      }
-      localStorage.setItem("so-cart", JSON.stringify(cart));
-    });
+  renderProductDetails() {
+    const container = document.querySelector(".product-detail");
+    container.innerHTML = `
+      <h3>${this.product.Brand?.Name || ""}</h3>
+      <h2 class="divider">${this.product.NameWithoutBrand}</h2>
+      <img src="${this.product.Image}" alt="${this.product.Name}">
+      <p class="product-card__price">$${this.product.FinalPrice}</p>
+      <p class="product__color">${this.product.Colors[0].ColorName}</p>
+      <p class="product__description">${this.product.DescriptionHtmlSimple}</p>
+      <div class="product-detail__add">
+        <button id="addToCart" data-id="${this.product.Id}">Add to Cart</button>
+      </div>
+    `;
   }
 }
