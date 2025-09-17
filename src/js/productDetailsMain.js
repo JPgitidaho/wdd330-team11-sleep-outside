deaa7e25cf0469ddc41b440162438bae50b6ee29
JPgitidaho <juanichile2015@gmail.com>
2025-09-17 10:04:34 -0300

    feat:Actualization dynamic product detail page implemented (Week 2)

--- src/js/productDetailsMain.js
+++ src/js/productDetailsMain.js
@@ -1,8 +1,12 @@
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData("tents");
-const element = document.querySelector("#product-detail");
-const product = new ProductDetails("tents", dataSource, element);
+const productId = getParam("product");
 
+const dataSource = new ProductData("tents");
+
+const element = document.querySelector("main.divider");
+
+const product = new ProductDetails(productId, dataSource, element);
 product.init();
