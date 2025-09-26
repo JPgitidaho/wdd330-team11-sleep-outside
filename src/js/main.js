a8f01187168f3b9528ee776065835dda6ff1af61
JPgitidaho <juanichile2015@gmail.com>
2025-09-26 20:04:39 -0300

    actualization individual tasks 04

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,11 @@
 import { loadHeaderFooter } from "./utils.mjs";
-loadHeaderFooter();
+import Alert from "./Alert.js";
+import { initCartBadge } from "./cartBadge.mjs";
+
+async function init() {
+  await loadHeaderFooter();
+  initCartBadge();
+  const alerts = new Alert("/json/alerts.json");
+  alerts.load();
+}
+init();
